17a24e14b9a6fb4ba19c71c705b75503e4a6d812
sriram-ravi705 <sriramravi477@gmail.com>
2025-02-04 20:39:21 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
-    @RequestMapping("/welcome")
+    @RequestMapping("/")
     public String index() {
         return "Greetings from Springboot..!!!";
     }
